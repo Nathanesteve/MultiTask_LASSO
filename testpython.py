0b13b3468e0dbb65b09b2404528a2690edcc6170
Mohamed Fattouhy <m.fattouhy@hotmail.com>
2021-02-10 21:55:08 +0100

    fix the conflic by removing 123

--- testpython.py
+++ testpython.py
@@ -3,11 +3,7 @@ import matplotlib.pyplot as plt
 from math import factorial
 
 c = 0
-for i in range(100):
-<<<<<<< HEAD
-    c = c + 1/(factorial(i))+123
-=======
-    c = c + 1/(factorial(i))+100
->>>>>>> b859fcf4a654b7b10edfd95519dede48cce2359c
 
+for i in range(100):
+	c = c + 1/(factorial(i))+100
 print(c)
